a8dcb618dda39fb65a7e6f6a09427e8cd5f425ff
pofider <honza.pofider@seznam.cz>
2015-02-28 12:52:18 +0100

    add comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+/*!
+ * Copyright(c) 2014 Jan Blaha (pofider)
+ *
+ * Simple OData server with adapters for mongodb and nedb
+ */
+
 var ODataServer = require("./lib/odataServer.js");
 
 module.exports = function(options) {
     return new ODataServer(options);
-}
+};
